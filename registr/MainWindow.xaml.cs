711d1c6d56697ac120a6ac978511a5fcefd81a31
user <user@DESKTOP-RA6T6M6>
2024-04-01 19:56:29 +0300

    всё

--- registr/MainWindow.xaml.cs
+++ registr/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
 
 namespace registr
 {
@@ -20,9 +22,92 @@ namespace registr
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string _email;
+        private static string _password;
+        private static int _code;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private static class MailSender
+        {
+            public static async Task SendMail(string recipient)
+            {
+                Random rnd = new Random();
+                _code = rnd.Next(1000, 9999);
+
+                MailAddress fromAddress = new MailAddress("brm280706@gmail.com", "Boss");
+                MailAddress toAddress = new MailAddress(recipient);
+
+                using (MailMessage message = new MailMessage(fromAddress, toAddress))
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    message.Subject = "Добро пожаловать!";
+                    message.Body = $"<h1>Код подтверждения:<br>{_code}</h1><br><h1>Данные для входа в аккаунт</h1><p>Почта: {_email}<br>Пароль: {_password}</p>";
+                    message.IsBodyHtml = true;
+
+                    smtpClient.Host = "smtp.gmail.com";
+                    smtpClient.Port = 587;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+
+                    smtpClient.Credentials = new NetworkCredential(fromAddress.Address, "woyo zwzk kyoc ldtb");
+
+                    await smtpClient.SendMailAsync(message);
+                }
+            }
+        }
+
+        private void SignInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmailTB.Text.Trim() == String.Empty || PasswordTB.Password.Trim() == String.Empty)
+            {
+                if (EmailTB.Text.Trim() == String.Empty)
+                    EmailTB.Background = Brushes.LightPink;
+                if (PasswordTB.Password.Trim() == String.Empty)
+                    PasswordTB.Background = Brushes.LightPink;
+                MessageBox.Show("Все поля должны быть заполнены!");
+                PasswordTB.Background = Brushes.White;
+                EmailTB.Background = Brushes.White;
+                return;
+            }
+
+            if (!EmailTB.Text.Contains("@"))
+            {
+                EmailTB.Background = Brushes.LightPink;
+                MessageBox.Show("Неверный формат почты!");
+                EmailTB.Clear();
+                EmailTB.Background = Brushes.White;
+                return;
+            }
+
+            EmailTB.Background = Brushes.LightGreen;
+            PasswordTB.Background = Brushes.LightGreen;
+
+            _email = EmailTB.Text;
+            _password = PasswordTB.Password;
+            SendMessage();
+
+            MessageBox.Show($"Данные для входа отправлены на почту {EmailTB.Text}");
+
+            EmailTB.Clear();
+            PasswordTB.Clear();
+            PasswordTB.Background = Brushes.White;
+            EmailTB.Background = Brushes.White;
+
+            new Window1(_code).ShowDialog();
+        }
+
+        private async Task SendMessage()
+        {
+            await MailSender.SendMail(_email);
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
---
+++ registr/Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace registr
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private static int _code;
+        public Window1(int code)
+        {
+            InitializeComponent();
+            _code = code;
+        }
+
+        private void OKBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (CodeTB.Text.Trim() == String.Empty)
+            {
+                CodeTB.Background = Brushes.LightPink;
+                MessageBox.Show("Заполните поле!");
+                CodeTB.Background = Brushes.White;
+            }
+
+            if (Convert.ToInt32(CodeTB.Text) == _code)
+            {
+                CodeTB.Background = Brushes.LightGreen;
+                MessageBox.Show("Email подтвержден!");
+                this.Close();
+            }
+            else
+            {
+                CodeTB.Background = Brushes.LightPink;
+                MessageBox.Show("Неверный код!");
+                CodeTB.Clear();
+            }
+        }
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
